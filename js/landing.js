daf93349b3a943f9c4e08482229425c02f20e75e
sssssqew <sunrise.in.my.dream@gmail.com>
2023-07-08 15:29:58 +0900

    페이지 모드 & 스크롤 라이브러리

---
+++ js/landing.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", (event) => {
+  // 테마변경 (다크모드/ 일반모드)
+  const mode = document.querySelector('.mode')
+  const icons = mode.querySelectorAll('.fa-solid')
+  const header = document.querySelector('header')
+
+  mode.addEventListener('click', (event) => {
+    document.body.classList.toggle('dark')
+    header.classList.toggle('dark')
+    
+    for(const icon of icons){
+      icon.classList.contains('active') ? 
+        icon.classList.remove('active') 
+        : icon.classList.add('active')
+    }
+  })
+})
