c82984dff67465833e38b89c5708acb80479690a
sssssqew <sunrise.in.my.dream@gmail.com>
2023-07-08 18:12:30 +0900

    네비게이션 메뉴 클릭시 해당 섹션으로 스크롤하기

--- js/landing.js
+++ js/landing.js
@@ -30,4 +30,26 @@ window.addEventListener("load", (event) => {
     history.pushState({}, "", `#`); // URL 주소 변경 
     scroller.setScrollPosition({top: 0, behavior: 'smooth'}) 
   })
+
+  // 네비게이션 메뉴 클릭시 해당 섹션으로 스크롤하기
+  const sections = document.querySelectorAll('section:not(.footer)') // 푸터를 제외한 section 엘리먼트 조회
+  const nav = document.querySelector('.navbar ul')
+
+  nav.querySelectorAll('li a').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+      const section = this.getAttribute('href') // 네비게이션 메뉴에서 클릭한 섹션 
+      const offsetToElementFromViewport = document.querySelector(section).getBoundingClientRect().top // 브라우저 상단에서 엘리먼트까지의 거리
+
+      if(!scroller.getScrollState()){ // 스크롤링이 멈춘 경우만 해당 섹션으로 스크롤링
+        event.preventDefault() // 부드러운 스크롤링 
+        history.pushState({}, "", `${section}`);  // URL 주소에 #about, #story, #contact 과 같은 파라미터 추가해서 URL 변경하기
+
+        const offsetToElementFromDocument = offsetToElementFromViewport + scroller.getScrollPosition() // 문서 상단에서 엘리먼트까지의 거리 
+        scroller.setScrollPosition({
+          top: offsetToElementFromDocument - header.offsetHeight - 10, // 헤더높이에서 10px 아래 위치로 스크롤링 
+          behavior: "smooth",
+        })
+      }
+    });
+  });
 })
