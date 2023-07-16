3c30eed031606e89a76e81c4c31fb5101b87235f
sssssqew <sunrise.in.my.dream@gmail.com>
2023-07-16 19:16:03 +0900

    포스트 페이지 - 파일업로드 & 파일 프리뷰 기능 구현하기

--- js/post.js
+++ js/post.js
@@ -53,4 +53,99 @@ window.addEventListener("load", (event) => {
       tagList.removeChild(event.target.parentElement) // 클릭이 발생한 a 요소의 부모요소인 li 태그 삭제
     }
   })
+
+  // 파일입력 처리하기
+  postContents.focus() // 첫로딩때 커서 보여주기
+  postContents.insertAdjacentElement('afterbegin', createNewline()) // 첫번째 줄에 새로운 공백라인 추가 
+  let lastCaretLine = postContents.firstChild // 아웃포커싱될때 첫번째 줄을 마지막 커서 위치로 설정
+  const uploadInput = document.querySelector('.upload input')
+  uploadInput.addEventListener('change', function(event){
+    const files = this.files
+
+    if(files.length > 0){
+      for(const file of files){
+        const fileType = file.type 
+
+        if(fileType.includes('image')){
+          console.log('image')
+          const img = document.createElement('img')   
+          img.src = URL.createObjectURL(file) // 업로드한 파일의 임시경로 (이미지 경로)
+          lastCaretLine = addFileToCurrentLine(lastCaretLine, img) // 편집기의 마지막 커서 위치에 파일 추가 
+        }else if(fileType.includes('video')){
+          console.log('video')
+          const video = document.createElement('video')
+          video.className = 'video-file'
+          video.controls = true
+          video.src = URL.createObjectURL(file)
+          lastCaretLine = addFileToCurrentLine(lastCaretLine, video)
+        }else if(fileType.includes('audio')){
+          console.log('audio')
+          const audio = document.createElement('audio')
+          audio.className = 'audio-file'
+          audio.controls = true
+          audio.src = URL.createObjectURL(file)
+          lastCaretLine = addFileToCurrentLine(lastCaretLine, audio)
+        }else{
+          console.log('file')
+          const div = document.createElement('div')
+          div.className = 'normal-file'
+          div.contentEditable = false 
+          div.innerHTML = `
+            <div class='file-icon'>
+              <span class="material-icons">folder</span>
+            </div>
+            <div class='file-info'>
+              <h3>${getFileName(file.name, 70)}</h3>
+              <p>${getFileSize(file.size)}</p>
+            </div>
+          `
+          lastCaretLine = addFileToCurrentLine(lastCaretLine, div) // 에디터에 파일추가 및 파일이 추가될때마다 커서위치 업데이트하기
+        }
+      }
+      // 커서위치를 맨 마지막으로 추가된 파일 아래쪽에 위치시키기
+      const selection = document.getSelection()
+      selection.removeAllRanges()
+
+      console.log(lastCaretLine)
+      const range = document.createRange()
+      range.selectNodeContents(lastCaretLine)
+      range.collapse()
+      selection.addRange(range) 
+
+      postContents.focus() // 파일을 모두 업로드한 후 커서 보여주기
+    }
+  })
+  // blur일때 마지막 커서 위치의 엘리먼트 저장하기
+  postContents.addEventListener('blur', function(event){ 
+    lastCaretLine = document.getSelection().anchorNode // 편집기 내부 커서가 위치한 곳의 엘리먼트 
+    console.log(lastCaretLine.parentNode, lastCaretLine, lastCaretLine.length)
+  })
+
+  function createNewline(){
+    const newline = document.createElement('div')
+    newline.innerHTML = `<br/>`
+    return newline
+  }
+  // blur 일때 마지막 커서 위치 바로 아래쪽에 새로운 줄을 생성하고 파일을 추가함
+  // 파일 추가후 다시 아래쪽에 새로운 줄 생성함
+  function addFileToCurrentLine(line, file){
+    if(line.nodeType === 3) line = line.parentNode // 커서 위치에 글자가 있는 경우 line 은 텍스트노드기 때문에 insertAdjacentElement 적용하지 못함 (부모는 div 엘리먼트라 적용가능)
+    line.insertAdjacentElement("afterend", createNewline())   // 추가
+    line.nextSibling.insertAdjacentElement("afterbegin", file)  
+    line.nextSibling.insertAdjacentElement("afterend", createNewline())
+
+    return line.nextSibling.nextSibling // 추가된 파일 아래쪽에 새로 생성된 줄 
+  }
+  function getFileName(name, limit){
+    return name.length > limit ? `${name.slice(0, limit)}... ${name.slice(name.lastIndexOf('.'), name.length)}` : name
+}
+  function getFileSize(number) {
+    if(number < 1024) {
+      return number + 'bytes';
+    } else if(number >= 1024 && number < 1048576) {
+      return (number/1024).toFixed(1) + 'KB';
+    } else if(number >= 1048576) {
+      return (number/1048576).toFixed(1) + 'MB';
+    }
+  }
 })
